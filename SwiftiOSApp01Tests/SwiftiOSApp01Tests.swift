bcefb317ea2fd628cc8981ae3490713785685393
Brian <brian.pfeil@gmail.com>
2014-06-02 22:08:08 -0400

    Initial Commit

---
+++ SwiftiOSApp01Tests/SwiftiOSApp01Tests.swift
@@ -0,0 +1,35 @@
+//
+//  SwiftiOSApp01Tests.swift
+//  SwiftiOSApp01Tests
+//
+//  Created by Pfeil, Brian on 6/2/14.
+//  Copyright (c) 2014 Pfeil, Brian. All rights reserved.
+//
+
+import XCTest
+
+class SwiftiOSApp01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
